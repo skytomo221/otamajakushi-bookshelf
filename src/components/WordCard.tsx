036d101527abfec3585003d7de9c04de49a9c707
skytomo <tomo1kawa.fullerene@gmail.com>
2021-03-23 19:30:06 +0900

    Add word card

--- src/components/WordCard.tsx
+++ src/components/WordCard.tsx
@@ -1,13 +1,18 @@
 import {
+  Box,
   Card,
   CardContent,
   Chip,
   createStyles,
+  Divider,
   makeStyles,
   Theme,
   Typography,
 } from '@material-ui/core';
+import { Content } from 'otamajakushi/dist/Content';
+import { Relation } from 'otamajakushi/dist/Relation';
 import { Translation } from 'otamajakushi/dist/Translation';
+import { Variation } from 'otamajakushi/dist/Variation';
 import { Word } from 'otamajakushi/dist/Word';
 import React from 'react';
 
@@ -22,6 +27,15 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     translationTitle: {
       margin: theme.spacing(0.25),
+    },
+    content: {
+      margin: theme.spacing(0.75),
+      '&:last-child': {
+        marginBottom: theme.spacing(0),
+      },
+    },
+    divider: {
+      margin: theme.spacing(0.5),
     },
   }),
 );
@@ -46,18 +60,78 @@ export default function WordCard(props: Props): JSX.Element {
           </span>
         </Typography>
         {word.translations.map((translation: Translation) => (
-          <>
-            <Typography variant="body2">
-              <Chip
-                variant="outlined"
-                size="small"
-                label={translation.title}
-                className={classes.translationTitle}
-              />
-              {translation.forms.join(', ')}
-            </Typography>
-          </>
+          <Typography key={translation.title} variant="body2">
+            <Chip
+              variant="outlined"
+              size="small"
+              label={translation.title}
+              className={classes.translationTitle}
+              component="h3"
+            />
+            {translation.forms.join(', ')}
+          </Typography>
         ))}
+        {word.contents.length > 0 ? (
+          <Divider className={classes.divider} />
+        ) : (
+          ''
+        )}
+        {word.contents.map((content: Content) => (
+          <Box key={content.title} className={classes.content}>
+            <Typography variant="h6" component="h3">
+              {content.title}
+            </Typography>
+            <Typography variant="body2">{content.text}</Typography>
+          </Box>
+        ))}
+        {word.variations.length > 0 ? (
+          <>
+            <Divider className={classes.divider} />
+            <Box className={classes.content}>
+              <Typography variant="h6" component="h3">
+                変化形
+              </Typography>
+              {word.variations.map((variation: Variation) => (
+                <Typography key={variation.title} variant="body2">
+                  <Chip
+                    variant="outlined"
+                    size="small"
+                    label={variation.title}
+                    className={classes.translationTitle}
+                    component="h3"
+                  />
+                  {variation.form}
+                </Typography>
+              ))}
+            </Box>
+          </>
+        ) : (
+          ''
+        )}
+        {word.relations.length > 0 ? (
+          <>
+            <Divider className={classes.divider} />
+            <Box className={classes.content}>
+              <Typography variant="h6" component="h3">
+                関連項目
+              </Typography>
+              {word.relations.map((relation: Relation) => (
+                <Typography key={relation.title} variant="body2">
+                  <Chip
+                    variant="outlined"
+                    size="small"
+                    label={relation.title}
+                    className={classes.translationTitle}
+                    component="h3"
+                  />
+                  {relation.entry.form}
+                </Typography>
+              ))}
+            </Box>
+          </>
+        ) : (
+          ''
+        )}
       </CardContent>
     </Card>
   );
