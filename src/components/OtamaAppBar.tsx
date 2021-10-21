a408c59349d527b4a819fb17e0ca6416c3950044
skytomo <tomo1kawa.fullerene@gmail.com>
2021-10-22 08:33:42 +0900

    Update OtamaAppBar.tsx

--- src/components/OtamaAppBar.tsx
+++ src/components/OtamaAppBar.tsx
@@ -29,84 +29,9 @@ import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
 import { mdiWindowMaximize } from '@mdi/js';
 import Icon from '@mdi/react';
 import React from 'react';
+import defaultLightTheme from '../extension/defaultLightTheme';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    appBar: {
-      margin: 0,
-      '-webkit-app-region': 'drag',
-      position: 'fixed',
-    },
-    grow: {
-      flexGrow: 1,
-    },
-    menuIconButton: {
-      marginRight: theme.spacing(2),
-      '-webkit-app-region': 'no-drag',
-    },
-    iconButton: {
-      '-webkit-app-region': 'no-drag',
-    },
-    title: {
-      display: 'none',
-      textOverflow: 'ellipsis',
-      overflow: 'hidden',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block',
-      },
-      minWidth: '100px',
-    },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginRight: theme.spacing(2),
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    inputRoot: {
-      color: 'inherit',
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('md')]: {
-        width: '20ch',
-      },
-    },
-    sectionDesktop: {
-      display: 'none',
-      [theme.breakpoints.up('xs')]: {
-        display: 'flex',
-        flexShrink: 1,
-      },
-    },
-    scrollTop: {
-      position: 'fixed',
-      bottom: theme.spacing(2),
-      right: theme.spacing(2),
-    },
-  }),
-);
+const useStyles = defaultLightTheme().styles;
 
 type ElevationScrollProps = {
   children: React.ReactElement;
