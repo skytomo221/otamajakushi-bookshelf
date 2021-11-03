316c6551d815873fc6273dcdc8f19a85d40818f5
skytomo <tomo1kawa.fullerene@gmail.com>
2021-11-03 17:24:09 +0900

    Goodbye Electron 👋

--- src/actions/SearchWordActions.ts
+++
@@ -1,9 +0,0 @@
-import { actionCreatorFactory } from 'typescript-fsa';
-
-const actionCreator = actionCreatorFactory('search-word-action');
-
-export const changeSearchWordAction = actionCreator<Partial<string>>(
-  'change-search-word',
-);
-
-export default changeSearchWordAction;
--- src/states/Bookshelf.ts
+++
@@ -1,5 +0,0 @@
-import Otm from 'otamajakushi/dist/Otm';
-
-export default interface Bookshelf {
-  books: Otm.Otm[];
-}
