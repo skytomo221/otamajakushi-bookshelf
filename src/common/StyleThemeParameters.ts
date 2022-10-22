8da3b2c8d6e1f7d192a028258477c19105265103
skytomo <tomo1kawa.fullerene@gmail.com>
2022-10-22 21:54:24 +0900

    Add style theme

---
+++ src/common/StyleThemeParameters.ts
@@ -0,0 +1,12 @@
+export default interface StyleThemeParameters {
+  menuBar: string;
+  statuBar: string;
+  editor: string;
+  h2: string;
+  h3: string;
+  h4: string;
+  h5: string;
+  h6: string;
+  lg: string;
+  base: string;
+}
