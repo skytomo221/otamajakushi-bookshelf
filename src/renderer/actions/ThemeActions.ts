5254427c65ca4169e93d323ce96c849510303ea8
skytomo <tomo1kawa.fullerene@gmail.com>
2022-10-23 11:20:45 +0900

    Add StyleThemeParameters

--- src/renderer/actions/ThemeActions.ts
+++ src/renderer/actions/ThemeActions.ts
@@ -1,10 +1,13 @@
 import { actionCreatorFactory } from 'typescript-fsa';
 
+import StyleThemeParameters from '../../common/StyleThemeParameters';
 import ThemeParameter from '../states/ThemeParameter';
 
 const actionCreator = actionCreatorFactory('change-theme');
 
-export const changeThemeAction =
-  actionCreator<ThemeParameter>('change-theme');
+export const changeThemeAction = actionCreator<ThemeParameter>('change-theme');
+
+export const applyStyleThemeAction =
+  actionCreator<StyleThemeParameters>('apply-style-theme');
 
 export default changeThemeAction;
--- src/renderer/reducers/ThemeReducer.ts
+++ src/renderer/reducers/ThemeReducer.ts
@@ -1,16 +1,36 @@
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 
-import { changeThemeAction } from '../actions/ThemeActions';
+import {
+  applyStyleThemeAction,
+  changeThemeAction,
+} from '../actions/ThemeActions';
 import ThemeParameter from '../states/ThemeParameter';
 
 const initTheme: ThemeParameter = {
   spacing: 8,
+  style: {
+    main: '',
+    menuBar: '',
+    statuBar: '',
+    editor: '',
+    h2: 'text-5xl',
+    h3: 'text-4xl',
+    h4: 'text-3xl',
+    h5: 'text-2xl',
+    h6: 'text-xl',
+    lg: 'text-lg',
+    base: 'text-base',
+  },
 };
 
 const themeReducer = reducerWithInitialState<ThemeParameter>(initTheme)
   .case(changeThemeAction, (state, payload) => ({
     ...state,
     ...payload,
+  }))
+  .case(applyStyleThemeAction, (state, style) => ({
+    ...state,
+    style,
   }))
   .build();
 
