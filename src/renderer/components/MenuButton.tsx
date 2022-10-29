8ae112bd83590d9d1a477f97bbb7a22a5eb09716
skytomo <tomo1kawa.fullerene@gmail.com>
2022-10-29 23:25:04 +0900

    Add MenuButton

---
+++ src/renderer/components/MenuButton.tsx
@@ -0,0 +1,20 @@
+import ButtonUnstyled, { ButtonUnstyledProps } from '@mui/base/ButtonUnstyled';
+import * as React from 'react';
+
+// eslint-disable-next-line react/display-name
+const MenuButton = React.forwardRef(
+  (props: ButtonUnstyledProps, ref: React.ForwardedRef<HTMLButtonElement>) => (
+    <ButtonUnstyled
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+      componentsProps={{
+        root: {
+          className: `flex [-webkit-app-region:no-drag] p-1.5 text-sm`,
+        },
+      }}
+      ref={ref}
+    />
+  ),
+);
+
+export default MenuButton;
