d4a2af54d6a04c3367b46c0f8abdb906aea2eaeb
skytomo <tomo1kawa.fullerene@gmail.com>
2022-11-05 00:56:46 +0900

    Add OtamaDefaultTheme

---
+++ src/main/OtamaDefaultTheme.ts
@@ -0,0 +1,28 @@
+import { StyleThemeProperties } from '../common/ExtensionProperties';
+import StyleTheme from '../common/StyleTheme';
+import StyleThemeParameters from '../common/StyleThemeParameters';
+
+export default class OtamaDefaultTheme extends StyleTheme {
+  public properties: StyleThemeProperties = {
+    name: 'Otama Default Theme',
+    id: 'otama-default-theme',
+    version: '0.1.0',
+    type: 'style-theme',
+    author: 'skytomo221',
+  };
+
+  readonly style = (): StyleThemeParameters => ({
+    main: '',
+    menuBar: 'bg-blue-500',
+    statuBar: 'bg-slate-300',
+    editor: '',
+    h2: 'text-5xl',
+    h3: 'text-4xl',
+    h4: 'text-3xl',
+    h5: 'text-2xl',
+    h6: 'text-xl',
+    lg: 'text-lg',
+    base: 'text-base',
+    'Menu.root': 'bg-slate-100 shadow-md m-1 px-2 py-1',
+  });
+}
