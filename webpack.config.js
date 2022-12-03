c993c31d8eb7838376ec3b902b265ce35c627ade
skytomo <tomo1kawa.fullerene@gmail.com>
2022-12-04 01:48:44 +0900

    Code golf ⛳

--- webpack.config.js
+++ webpack.config.js
@@ -3,77 +3,13 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 const outputPath = path.join(__dirname, 'dist');
-const mainConfig = {
-  target: 'electron-main',
-  entry: './src/main/index.ts',
+const common = {
   cache: true,
   mode: 'development',
   devtool: 'source-map',
   output: {
     path: outputPath,
     filename: 'main.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        enforce: 'pre',
-        loader: 'eslint-loader',
-      },
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-};
-const preloadConfig = {
-  target: 'electron-preload',
-  entry: './src/renderer/preload.ts',
-  cache: true,
-  mode: 'development',
-  devtool: 'source-map',
-  output: {
-    path: outputPath,
-    filename: 'preload.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        enforce: 'pre',
-        loader: 'eslint-loader',
-      },
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-};
-const rendererConfig = {
-  target: 'electron-renderer',
-  entry: './src/renderer/index.tsx',
-  cache: true,
-  mode: 'development',
-  devtool: 'source-map',
-  output: {
-    path: outputPath,
-    filename: 'renderer.js',
   },
   module: {
     rules: [
@@ -104,6 +40,21 @@ const rendererConfig = {
       filename: './css/style.css',
     }),
   ],
+}
+const mainConfig = {
+  ...common,
+  target: 'electron-main',
+  entry: './src/main/index.ts',
+};
+const preloadConfig = {
+  ...common,
+  target: 'electron-preload',
+  entry: './src/renderer/preload.ts',
+};
+const rendererConfig = {
+  ...common,
+  target: 'electron-renderer',
+  entry: './src/renderer/index.tsx',
 };
 
 module.exports = [mainConfig, preloadConfig, rendererConfig];
